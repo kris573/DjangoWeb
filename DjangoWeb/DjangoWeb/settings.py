a8641e90625964ea3dc3545aafaf56416f3474a4
kris573 <1176623625@qq.com>
2020-06-22 21:22:30 +0800

    一个新的分支

--- DjangoWeb/DjangoWeb/settings.py
+++ DjangoWeb/DjangoWeb/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'testdjango.urls'
+ROOT_URLCONF = 'DjangoWeb.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'testdjango.wsgi.application'
+WSGI_APPLICATION = 'DjangoWeb.wsgi.application'
 
 
 # Database
